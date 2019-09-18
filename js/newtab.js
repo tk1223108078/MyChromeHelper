abc622d20c39adce9dc4124d9576a06e8368b559
kai <oncwnuNOWVIoLQaRVFMOUqbbJHmU@git.weixin.qq.com>
2019-09-18 21:01:54 +0800

    完成快捷链接移入移出时的动画效果

--- js/newtab.js
+++ js/newtab.js
@@ -1,28 +1,84 @@
-// 鼠标移入
-function itemmouseover(item){
-    item.style.opacity = 0.5;
+function ShortCutItemClick(index){
+    alert("itemclick"+index);
 }
 
-// 鼠标移出
-function itemouseout(item){
-    item.style.opacity = 1;
-}
-
-// 为Item设置各种事件
-function AddItemtEvent(){
-    var items = document.getElementsByClassName("common-page-item");
-    for(let index in items){
-        item = items[index];
-        item.setAttribute('id', 'pageitem-' + index);
-        var itembyid = document.getElementById('pageitem-' + index);
-        itembyid.onmousemove = itemmouseover(itembyid);
-        itembyid.onmouseout = itemouseout(itembyid);
+function ShortCutItemMouseEnter(index){
+    var Item  = document.getElementById("ShortCut"+index);
+    if(Item){
+        Item.setAttribute("class", "common-page-item-select");
     }
+    var Modify  = document.getElementById("ShortCutModify"+index);
+    if(Modify){
+        Modify.style.display="block";
+    }
+}
+
+function ShortCutItemMouseLeave(index){
+    var Item  = document.getElementById("ShortCut"+index);
+    if(Item){
+        Item.setAttribute("class", "common-page-item");
+    }
+    var Modify  = document.getElementById("ShortCutModify"+index);
+    if(Modify){
+        Modify.style.display="none";
+    }
+}
+
+function ShortCutModifyClick(index){
+    alert("ModifyClick"+index);
+    // 阻止响应div的点击事件
+    event.stopPropagation();
+}
+
+// 添加快捷方式Item
+function AddShortCutItem(index){
+    // Item
+    var divCommonPageItem = document.createElement("div");
+    divCommonPageItem.setAttribute("class", "common-page-item");
+    divCommonPageItem.setAttribute("id", "ShortCut"+index);
+    // 点击事件
+    divCommonPageItem.setAttribute("onclick", "ShortCutItemClick("+index+")");
+    divCommonPageItem.setAttribute("onmouseenter", "ShortCutItemMouseEnter("+index+")");
+    divCommonPageItem.setAttribute("onmouseleave", "ShortCutItemMouseLeave("+index+")");
+
+    // icon
+    var divCommonPageItemIcon = document.createElement("div");
+    divCommonPageItemIcon.setAttribute("class", "common-page-item-icon");
+    var imgCommonPageItemIcon = document.createElement("img");
+    imgCommonPageItemIcon.setAttribute("src", "../img/search.png");
+    divCommonPageItemIcon.appendChild(imgCommonPageItemIcon);
+
+    // label
+    var divCommonPageItemLabel = document.createElement("div");
+    divCommonPageItemLabel.setAttribute("class", "common-page-item-label");
+    var spanCommonPageItemLabel = document.createElement("span");
+    spanCommonPageItemLabel.textContent = "测试";
+    divCommonPageItemLabel.appendChild(spanCommonPageItemLabel);
+
+    // modify
+    var divCommonPageItemModify = document.createElement("div");
+    divCommonPageItemModify.setAttribute("class", "common-page-item-modify");
+    divCommonPageItemModify.setAttribute("title", "修改快捷方式");
+    divCommonPageItemModify.setAttribute("aria-label", "修改快捷方式");
+    divCommonPageItemModify.setAttribute("id", "ShortCutModify"+index);
+    // 点击事件
+    divCommonPageItemModify.setAttribute("onclick", "ShortCutModifyClick("+index+")");
+
+    // 填充到Item的div中
+    divCommonPageItem.appendChild(divCommonPageItemIcon);
+    divCommonPageItem.appendChild(divCommonPageItemLabel);
+    divCommonPageItem.appendChild(divCommonPageItemModify);
+
+    // 添加到主页面上
+    document.getElementById("common-pages").appendChild(divCommonPageItem);
 }
 
 // 界面加载事件
 window.onload = function(){
     // 添加Item事件
-    AddItemtEvent();
+    for(var index = 0x0; index < 10; index++){
+        AddShortCutItem(index);
+    }
+
 }
 
