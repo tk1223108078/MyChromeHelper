1c02deda498df4a0d15849cb2bf0b6f83222f8f2
taokai <1223108078@qq.com>
2019-12-10 15:33:53 +0800

    将JS通用函数和谷歌浏览器插件中通用函数提取出来

---
+++ js/common/chrome.js
@@ -0,0 +1,23 @@
+// 获取当前选项卡ID
+function getCurrentTabId(callback)
+{
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
+    {
+        if(callback) callback(tabs.length ? tabs[0].id: null);
+    });
+}
+
+// 当前标签打开某个链接
+function openUrlCurrentTab(url)
+{
+    getCurrentTabId(tabId => {
+        chrome.tabs.update(tabId, {url: url});
+    })
+}
+
+// 新标签打开某个链接
+function openUrlNewTab(url){
+    chrome.tabs.create({
+        'url': url
+    });
+}
---
+++ js/common/common.js
@@ -0,0 +1,61 @@
+// 获取网络请求信息
+var getResponse = async function(url) {
+    let ok = 0;
+    let data = "";
+    try{
+        let response = await fetch(url, {
+            headers:{
+                'Set-Cookie': 'HttpOnly;Secure;SameSite=Strict'
+              }
+        });
+        if(response.ok && response.status == 200){
+            // 标记通讯成功，并且设置返回值
+            ok = 1;
+            data = await response.json();
+        }else{
+            data = "网络请求失败，ok="+response.ok+", status="+response.status;
+        }
+    }catch(error){
+        data = "发生异常, error="+error;
+    }
+    // 返回值
+    return {"ok":ok, "data":data};
+}
+
+// 设置元素显示还是隐藏
+function elementsSetDisplay(item, show){
+    if(item){
+        if(show == true){
+            item.style.display="block"
+        }else{
+            item.style.display="none"
+        }
+    }else{
+        console.error("[elementsSetDisplay] 需要设置的元素无效");
+    }
+}
+
+// 为元素设置响应事件
+function elementsSetEventFunction(item, eventName, fucntion){
+    if(item){
+        item.addEventListener(eventName, fucntion);
+    }else{
+        console.error("[elementsSetEventFunction] 需要设置的元素无效");
+    }
+}
+
+// 判断输入是否是URL
+function isUrl(str_url) {// 验证url
+    var strRegex = "^((https|http|ftp|rtsp|mms)?://)"
+    + "?(([0-9a-zA-Z_!~*'().&=+$%-]+: )?[0-9a-zA-Z_!~*'().&=+$%-]+@)?" // ftp的user@
+    + "(([0-9]{1,3}\.){3}[0-9]{1,3}" // IP形式的URL- 199.194.52.184
+    + "|" // 允许IP和DOMAIN（域名）
+    + "([0-9a-zA-Z_!~*'()-]+\.)*" // 域名- www.
+    + "([0-9a-zA-Z][0-9a-zA-Z-]{0,61})?[0-9a-zA-Z]\." // 二级域名
+    + "[a-zA-Z]{2,6})" // first level domain- .com or .museum
+    + "(:[0-9]{1,4})?" // 端口- :80
+    + "((/?)|" // a slash isn't required if there is no file name
+    + "(/[0-9a-zA-Z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+    var re = new RegExp(strRegex);
+    return re.test(str_url);
+}
