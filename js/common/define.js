9cbedceff1a72000c231202c341b9ad74da3b392
kai <oncwnuNOWVIoLQaRVFMOUqbbJHmU@git.weixin.qq.com>
2019-12-15 11:56:47 +0800

    修正历史记录的存储个数

--- js/common/define.js
+++ js/common/define.js
@@ -1,5 +1,6 @@
 var messgae_flag_key = "www.taosijie.com/mychromehelper"
 var localstorage_fastitemlist_key = "www.taosijie.com/mychromehelper/fastitemlist"
 var localstorage_historypagelist_key = "www.taosijie.com/mychromehelper/historypagelist"
-var historypagelist_maxsize = 10;
+// chrome的localstorage的存储大小约为5M，因此只记录1000条历史记录，应该没什么问题
+var historypagelist_maxsize = 1000;
 var historypagelist_clearsize = historypagelist_maxsize/10;
