04db9f6ce375fca6b70122f948cbc17b38e38010
tk1223108078 <woshitaokai@gmail.com>
2021-01-15 14:26:47 +0800

    添加复制Cookie的gon'n

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,20 @@
 
-chrome.tabs.query({url: UrlText}, function(tabs) {
+function copyCookie(){
+    var bg = chrome.extension.getBackgroundPage();
+    bg.showCookie();
+}
+
+// 界面加载事件
+window.onload = function(){
+    // 添加点击事件
+    var copyCookieBtn = document.getElementById("copyCookie");
+    copyCookieBtn.addEventListener("click", copyCookie);
+}
+
+/*chrome.tabs.query({url: UrlText}, function(tabs) {
     // 向content中发送消息
     // https://developer.chrome.com/extensions/tabs#method-sendMessage
     chrome.tabs.sendMessage(tabs[0].id, {}, function(response) {
         console.log(response);
     });
-});
+});*/
