8dea0e05bc3298cdf1b58799ad03eb047bf96b92
kai <oncwnuNOWVIoLQaRVFMOUqbbJHmU@git.weixin.qq.com>
2021-01-24 12:02:22 +0800

    完善了判断、收割、种植接口和流程，但是这边收割接口还是会失败，须待调查

--- js/background.js
+++ js/background.js
@@ -129,18 +129,81 @@ async function getCookieByDomain(url) {
     return { "ok": ok, "data": data };
 }
 
+// 获取植物状态
+async function getFarmHome() {
+    let getFarmHomeUrl = 'https://act.suning.com/act-wap-web/snfarm/gateway/getFarmHome.do?activityCode=snfarm';
+    let response = await getResponseWithCookie(getFarmHomeUrl);
+    console.log('getFarmHome Reponse=' + JSON.stringify(response));
+
+    // 检查通讯结果
+    if (response.ok != 1) {
+        return { result: 1, data: response.data };
+    }
+
+    // 检查返回结果
+    if (response.data.code != 1) {
+        return { result: 2, data: response.data };
+    }
+
+    return { result: 0, data: response.data.data };
+}
+
+
+// 收割成熟植物
+async function harvestCrops(cropId, fieldNo) {
+    let harvestCropsUrl = 'https://act.suning.com/act-wap-web/snfarm/gateway/harvestCrops.do?activityCode=snfarm&cropId=' + cropId + '&fieldNo=' + fieldNo + '&referenceURL=https%3A%2F%2Fc.m.suning.com%2FnewFarm2020.html%3Fsafp%3Df73ee1cf.wapindex7.113456729859.6%26safpn%3D10001%23%2F&channel=WAP HTTP/1.1';
+    let response = await getResponseWithCookie(harvestCropsUrl);
+    console.log("harvestCrops Reponse=" + JSON.stringify(response));
+
+    // 通讯失败
+    if (response.ok != 1) {
+        return { result: false, errMsg: '通讯失败' + response.data };
+    }
+    // 检查返回结果
+    if (response.data.code != 1) {
+        return { result: false, errMsg: '苏宁返回失败' + response.data.code };
+    }
+    return { result: true };
+}
+
+// 种植新植物
+async function plantCrops(cropId, fieldNo) {
+    let plantCropsUrl = 'https://act.suning.com/act-wap-web/snfarm/gateway/plantCrops.do?activityCode=snfarm&cropId=' + cropId + '&fieldNo=' + fieldNo;
+    let response = await getResponseWithCookie(plantCropsUrl);
+    console.log("plantCrops Reponse=" + JSON.stringify(response));
+
+    // 通讯失败
+    if (response.ok != 1) {
+        return { result: false, errMsg: '通讯失败' + response.data };
+    }
+    // 检查返回结果
+    if (response.data.code != 1) {
+        return { result: false, errMsg: '苏宁返回失败' + response.data.code };
+    }
+    return { result: true };
+}
+
 // 记录下上次请求失败的Cookie中的AuthId, 
 var RequestFailedForNoLoginAuthIdString = "";
+var QueryCount = 0x0;
+var plantCropId = '0001'; // 萝卜 0001 
 
 // 自动执行函数
 async function onAutoRun() {
-    let url = 'https://c.m.suning.com/newFarm2020.html';
-    // 貌似都不需要打开
-    // // 确保打开了苏宁农庄
-    // var isOpen = await isUrlOpen(url);
-    // if (isOpen == false) {
+    // 每隔五分钟才执行一次查询操作
+    // if (QueryCount % 100 != 0x0) {
     //     return;
     // }
+
+    // 添加计数
+    QueryCount++;
+    let url = 'https://c.m.suning.com/newFarm2020.html';
+
+    // 确保打开了苏宁农庄
+    var isOpen = await isUrlOpen(url);
+    if (isOpen == false) {
+        return;
+    }
 
     // 获取指定界面的
     var CookieResult = await getCookieByDomain(url);
@@ -153,11 +216,87 @@ async function onAutoRun() {
     if (CookieResult.data.has('authId') == false) {
         return;
     }
-
-    if ()
+    var authIdString = CookieResult.data.authId;
+    if (authIdString == RequestFailedForNoLoginAuthIdString) {
+        console.log("对比Authid认为登录已过期");
+    }
 
     // 打开了苏宁的网页并且登录了，符合查询的条件了
-        console.log("打开了苏宁的网站并且已经登录了，可以查询农场的收获情况了");
+    console.log("认为已经登录成功，且Cookie未过期");
+
+    // 获取苏宁农场信息
+    let getFarmHomeResponse = await getFarmHome();
+    if (getFarmHomeResponse.code == 0x1) {
+        console.log("获取土地情况网络通讯失败");
+        return;
+    } else if (getFarmHomeResponse.code == 0x2) {
+        // 登录过期，刷新下界面，等下次查询
+        if (getFarmHomeResponse.data.code == 0x2) {
+            // 记录下失败的AuthId， 并刷新界面
+            RequestFailedForNoLoginAuthIdString = authIdString;
+            refreshTabByUrl(url);
+        }
+        onsole.log("网站返回请求失败");
+        return;
+    }
+
+    // 输出信息
+    let userFieldList = getFarmHomeResponse.data.result.userFieldList;
+    for (var index = 0; index < userFieldList.length; index++) {
+        var userField = userFieldList[index];
+        var fieldNo = userField.fieldNo;
+        // 土地处于种植状态
+        if (userField.fieldStatus == 0x1) {
+            console.log("当前土地处于种植状态。no=", fieldNo);
+
+            // 判断成熟时间是否有效
+            if (userField.cropRipeTime == undefined) {
+                console.log("当前土地成熟时间为空。no=", fieldNo);
+                continue;
+            }
+
+            // 判断当前土地是否已经成熟
+            const curDate = Date.now();
+            let cropRipeTimeDate = Date.parse(userField.cropRipeTime);
+            if (curDate < cropRipeTimeDate) {
+                console.log("当前土地未成熟。no=", fieldNo);
+                console.log("curTime=", curDate.toString());
+                console.log("cropRipeTime=", cropRipeTimeDate.toString());
+                continue;
+            }
+
+            // 收获植物
+            let response = await harvestCrops(userField.cropId, fieldNo);
+            if (response.result == true) {
+                console.log("收获成功。no=", fieldNo);
+                console.log("cropId=", userField.cropId);
+            } else {
+                console.log("收获失败。no=", fieldNo);
+                console.log('errMgs=', response.errMsg);
+                refreshTabByUrl(url);
+            }
+
+            // 休息3秒
+            await sleep(3000);
+        }
+        // 土地处于闲置状态
+        else if (userField.fieldStatus == 0x0) {
+            // 种植新植物
+            console.log("当前土地处于荒芜状态。no=", fieldNo);
+            let response = await plantCrops(plantCropId, fieldNo);
+            if (response.result == true) {
+                console.log("种植成功。no=", fieldNo);
+                console.log("cropId=", plantCropId);
+            } else {
+                console.log("种植失败。no=", fieldNo);
+                console.log('errMgs=', response.errMsg);
+                refreshTabByUrl(url);
+            }
+
+            // 休息3秒
+            await sleep(3000);
+        }
+    }
 
 }
 
